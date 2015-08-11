3a08c11a1793a90c7b4c4a401ab87bb845728761
lzueclipse <lzueclipse@163.com>
2015-08-11 02:32:55 -0400

    STL map does not release small chunks of memory after destruction(glibc malloc behavior).

--- c_cpp/0001/malloc_test.cpp
+++ c_cpp/0001/malloc_test.cpp
@@ -25,24 +25,24 @@
 
 typedef struct
 {
-	unsigned char digest[16];
+    unsigned char digest[16];
 }md5_digest_t;
 
 void output_top()
 {
-	FILE *in;
+    FILE *in;
     char buf[LEN];
     char cmd[LEN];
    
     memset(cmd, 0, sizeof(cmd));
     snprintf(cmd, sizeof(cmd), "top -p %d -n 1  |grep %d", getpid(), getpid());
     if(!(in = popen(cmd, "r")))
-	{
-		exit(1);
+    {
+        exit(1);
     }
    
     while(fgets(buf, sizeof(buf), in)!=NULL)
-	{
+    {
        printf("%s", buf);
     }
 
@@ -51,30 +51,30 @@ void output_top()
 
 void print_md5(FP fp)
 {
-	int i;
-	char buf[33]={'\0'};
-	char tmp[3]={'\0'};
+    int i;
+    char buf[33]={'\0'};
+    char tmp[3]={'\0'};
 
-	for( i=0; i<16; i++ ){
-		snprintf(tmp, sizeof(tmp), "%02x", fp.inside_uchar[i]);
-		strcat(buf,tmp);
-	}
-	printf("%s\n",buf);
+    for( i=0; i<16; i++ ){
+        snprintf(tmp, sizeof(tmp), "%02x", fp.inside_uchar[i]);
+        strcat(buf,tmp);
+    }
+    printf("%s\n",buf);
 }
 
 void int_to_md5(uint64_t input, FP &output )
 {
-	char data[LEN] = {'\0'};
+    char data[LEN] = {'\0'};
     snprintf(data, sizeof(data), "%" PRIu64, input);
-	
-	MD5_CTX ctx;
-	MD5_Init(&ctx);
-	
-	MD5_Update(&ctx,data,strlen(data));
-	
-	MD5_Final(output.inside_uchar,&ctx);
+    
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+    
+    MD5_Update(&ctx,data,strlen(data));
+    
+    MD5_Final(output.inside_uchar,&ctx);
 
-	print_md5(output);
+    print_md5(output);
 }
 
 void test_map()
@@ -83,8 +83,8 @@ void test_map()
   std::list<uint64_t> my_list;
   FP my_fp;
   for(i = 0; i < MAXNUM; ++i) {
-	  int_to_md5(i, my_fp);
-	  print_md5(my_fp);
+      int_to_md5(i, my_fp);
+      print_md5(my_fp);
   }
   printf("Insert data into std::map, output of 'top':\n");
   output_top();
@@ -103,28 +103,28 @@ void test_mycache()
 
 void usage(char *prog)
 {
-	printf("Usage: %s [test_map | test_mycache]\n", prog);
-	exit(-1);
+    printf("Usage: %s [test_map | test_mycache]\n", prog);
+    exit(-1);
 }
 
 int main(int argc, char **argv) 
 {
     if(argc != 2)
     {
-		usage(argv[0]);
+        usage(argv[0]);
     }
 
     if(strcmp(argv[1], "test_map") == 0)
     {
-	    test_map();
+        test_map();
     }
     else if(strcmp(argv[1], "test_mycache") == 0)
     {
-	    test_mycache();
+        test_mycache();
     }
     else
     {
-	    usage(argv[0]);
+        usage(argv[0]);
     }
 
   return 0;
