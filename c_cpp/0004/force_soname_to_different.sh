1977ff832db305e5b9c7f002a37c27f15b2994e6
lzueclipse <lzueclipse@163.com>
2015-08-13 23:04:10 -0400

    Some test about diamond linking -- Shared libarary, load same lib 2 times, with same version & different version

--- c_cpp/0004/force_soname_to_different.sh
+++ c_cpp/0004/force_soname_to_different.sh
@@ -21,16 +21,16 @@ mv libopensource* ./opensource_v1
 gcc -Wl,-rpath=./opensource_v1 -L ./opensource_v1 -Wl,--default-symver -Wl,-soname,libvendor1.so -shared -o libvendor1.so vendor1.o -lopensource
 rm -f *.o
 
-#To generate ./opensource_v2/libopensource.so.1.0, libvendor2.so
+#To generate ./opensource_v2/libopensource.so.2.0, libvendor2.so
 #Use opensource_v2.c, vendor2.c
 cp opensource_v2.c opensource.c
 
 gcc -fPIC -c -o vendor2.o vendor2.c
 gcc -fPIC -c -o opensource.o opensource.c
-gcc -shared -fPIC -Wl,--default-symver -Wl,-soname,libopensource.so.1 -o libopensource.so.1.0 opensource.o
+gcc -shared -fPIC -Wl,--default-symver -Wl,-soname,libopensource.so.2 -o libopensource.so.2.0 opensource.o
 
-ln -s libopensource.so.1.0 libopensource.so
-ln -s libopensource.so.1.0 libopensource.so.1
+ln -s libopensource.so.2.0 libopensource.so
+ln -s libopensource.so.2.0 libopensource.so.2
 mkdir opensource_v2
 mv libopensource* ./opensource_v2
 
