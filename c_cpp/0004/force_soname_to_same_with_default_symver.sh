1949ebf984a70f33dcbabe09c212e2d6887f4078
lzueclipse <lzueclipse@163.com>
2015-08-13 23:54:44 -0400

    Some test about diamond linking -- Shared libarary, load same lib 2 times, with same version & different version

--- c_cpp/0004/force_soname_to_same_with_default_symver.sh
+++ c_cpp/0004/force_soname_to_same_with_default_symver.sh
@@ -12,7 +12,6 @@ cp opensource_v1.c opensource.c
 gcc -fPIC -c -o vendor1.o vendor1.c
 gcc -fPIC -c -o opensource.o opensource.c
 gcc -shared -fPIC -Wl,--default-symver -Wl,-soname,libopensource.so.1 -o libopensource.so.1.0 opensource.o
-#gcc -shared -fPIC -Wl,-soname,libopensource.so.1 -o libopensource.so.1.0 opensource.o
 
 ln -s libopensource.so.1.0 libopensource.so
 ln -s libopensource.so.1.0 libopensource.so.1
@@ -20,7 +19,6 @@ mkdir ./opensource_v1
 mv libopensource* ./opensource_v1
 
 gcc -Wl,-rpath=./opensource_v1 -L ./opensource_v1 -Wl,--default-symver -Wl,-soname,libvendor1.so -shared -o libvendor1.so vendor1.o -lopensource
-#gcc -Wl,-rpath=./opensource_v1 -L ./opensource_v1  -Wl,-soname,libvendor1.so -shared -o libvendor1.so vendor1.o -lopensource
 rm -f *.o
 
 #To generate ./opensource_v2/libopensource.so.1.0, libvendor2.so
@@ -30,7 +28,6 @@ cp opensource_v2.c opensource.c
 gcc -fPIC -c -o vendor2.o vendor2.c
 gcc -fPIC -c -o opensource.o opensource.c
 gcc -shared -fPIC -Wl,--default-symver -Wl,-soname,libopensource.so.1 -o libopensource.so.1.0 opensource.o
-#gcc -shared -fPIC  -Wl,-soname,libopensource.so.1 -o libopensource.so.1.0 opensource.o
 
 ln -s libopensource.so.1.0 libopensource.so
 ln -s libopensource.so.1.0 libopensource.so.1
@@ -38,7 +35,6 @@ mkdir opensource_v2
 mv libopensource* ./opensource_v2
 
 gcc -Wl,-rpath=./opensource_v2 -L ./opensource_v2 -Wl,--default-symver -Wl,-soname,libvendor2.so -shared -o libvendor2.so vendor2.o -lopensource
-#gcc -Wl,-rpath=./opensource_v2 -L ./opensource_v2 -Wl,-soname,libvendor2.so -shared -o libvendor2.so vendor2.o -lopensource
 rm -f *.o
 
 #main.c
