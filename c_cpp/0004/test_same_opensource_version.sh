214b43f57a1a6788ebd82dd03c67007e7114054b
lzueclipse <lzueclipse@163.com>
2015-08-13 22:46:57 -0400

    Deep dive into Diamond Linking --Shared libarary, load same lib 2 times, with same version & different version

--- c_cpp/0004/test_same_opensource_version.sh
+++ c_cpp/0004/test_same_opensource_version.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-set -x
 
 rm -f ./lib*
 rm -rf ./opensource_v1
@@ -9,38 +8,37 @@ rm -rf ./opensource_v2
 #To generate ./opensource_v1/libopensource.so.1.0, libvendor1.so
 #Use opensource_v1.c, vendor1.c
 cp opensource_v1.c opensource.c  
+
 gcc -fPIC -c -o vendor1.o vendor1.c
 gcc -fPIC -c -o opensource.o opensource.c
 gcc -shared -fPIC -Wl,--default-symver -Wl,-soname,libopensource.so.1 -o libopensource.so.1.0 opensource.o
-mkdir ./vendor1
-mv opensource.o ./vendor1
-mv libopensource.so.1.0 ./vendor1
-cd ./vendor1
+
 ln -s libopensource.so.1.0 libopensource.so
 ln -s libopensource.so.1.0 libopensource.so.1
-cd ..
-gcc -Wl,-rpath=./:./vendor1 -L. -L ./vendor1 -Wl,--default-symver -Wl,-soname,libvendor1.so -shared -o libvendor1.so vendor1.o -lopensource
-rm -f *.o
-rm -f ./vendor1/*.o
+mkdir ./opensource_v1
+mv libopensource* ./opensource_v1
 
-#To generate ./vendor2/libopensource.so.1.0, libvendor2.so
+gcc -Wl,-rpath=./opensource_v1 -L ./opensource_v1 -Wl,--default-symver -Wl,-soname,libvendor1.so -shared -o libvendor1.so vendor1.o -lopensource
+rm -f *.o
+
+#To generate ./opensource_v2/libopensource.so.1.0, libvendor2.so
 #Use opensource_v2.c, vendor2.c
 cp opensource_v2.c opensource.c
+
 gcc -fPIC -c -o vendor2.o vendor2.c
 gcc -fPIC -c -o opensource.o opensource.c
 gcc -shared -fPIC -Wl,--default-symver -Wl,-soname,libopensource.so.1 -o libopensource.so.1.0 opensource.o
-mkdir ./vendor2
-mv opensource.o ./vendor2
-mv libopensource.so.1.0 ./vendor2
-cd ./vendor2
+
 ln -s libopensource.so.1.0 libopensource.so
 ln -s libopensource.so.1.0 libopensource.so.1
-cd ..
-gcc -Wl,-rpath=./:./vendor2 -L. -L ./vendor2 -Wl,--default-symver -Wl,-soname,libvendor2.so -shared -o libvendor2.so vendor2.o -lopensource
+mv libopensource* ./opensource_v2
+
+gcc -Wl,-rpath=./opensource_v2 -L ./opensource_v2 -Wl,--default-symver -Wl,-soname,libvendor2.so -shared -o libvendor2.so vendor2.o -lopensource
 rm -f *.o
-rm -f ./vendor2/*.o
 
 #main.c
-gcc -Wl,-rpath=./:./vendor2:./vendor1 -o main.o main.c  -Wl,-rpath=. -L. -L./vendor1 -L./vendor2 -lvendor1 -lvendor2 -lopensource 
+gcc -Wl,-rpath=./:./vendor1:./vendor2 -o main  main.c  -Wl,-rpath=. -L. -L./vendor1 -lvendor1 -lvendor2 -lopensource 
 
-echo "success"
+#gcc -Wl,-rpath=./:./vendor1:./vendor2 -o main  main.c  -Wl,-rpath=. -L. -L./vendor1 -L./vendor2 -lvendor1 -lvendor2 -lopensource 
+
+echo "Complile success"
