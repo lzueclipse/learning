c78ed8b9f794a95a6beb45401cb80dbd5ffe8f1f
lzueclipse <lzueclipse@163.com>
2015-08-15 21:30:29 +0800

    Some test about diamond linking -- Shared libarary, load same lib at the same time,  with same version & different version

--- c_cpp/0004/main.c
+++ c_cpp/0004/main.c
@@ -1,8 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 extern void vendor1();
 extern void vendor2();
-int main()
+int main(int argc, char **argv)
 {
-    vendor1();
-    vendor2();
+    if(argc != 2)
+    {
+       printf("%s [normal | dlopen]\n", argv[0]);
+       exit(0);
+    }
+    
+    if(strcmp(argv[1], "normal") == 0)
+    {
+        vendor1();
+        vendor2();
+    }
+
+    if(strcmp(argv[1], "dlopen") == 0)
+    {
+
+    }
     return 0;
 }
