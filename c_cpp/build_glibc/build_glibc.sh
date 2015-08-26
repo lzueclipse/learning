472450b667372d4cf446eeaf5bbf39c490cdca0d
lzueclipse <lzueclipse@163.com>
2015-08-26 02:16:26 -0400

    STL map does not release small chunks of memory after destruction(glibc malloc behavior).

--- c_cpp/build_glibc/build_glibc.sh
+++ c_cpp/build_glibc/build_glibc.sh
@@ -14,9 +14,15 @@ mkdir -p $SRCDIR
 mkdir -p $DESTDIR
 mkdir -p $BUILDDIR
 
+##Get source code, and extract
 cd $SRCDIR
 wget http://ftp.gnu.org/gnu/glibc/glibc-2.17.tar.gz
 tar -zxvf glibc-2.17.tar.gz
+
+##Glibc build bug, Endless loop building misc/bits/syscall.d in glibc 2.17
+##https://sourceware.org/bugzilla/show_bug.cgi?id=15711
+cd $SRCDIR/glibc-2.17
+sed "798 itouch -d '0.5 seconds ago' \$\@" -i Makerules
 
 cd $BUILDDIR
 $SRCDIR/glibc-2.17/configure --prefix=/usr
