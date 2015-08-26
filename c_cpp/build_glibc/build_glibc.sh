ead26c3ca4cf7ec057a33c37b78f1531b730f968
lzueclipse <lzueclipse@163.com>
2015-08-25 23:06:50 -0400

    STL map does not release small chunks of memory after destruction(glibc malloc behavior).

--- c_cpp/build_glibc/build_glibc.sh
+++ c_cpp/build_glibc/build_glibc.sh
@@ -3,8 +3,8 @@
 set -e
 
 SRCDIR=$HOME/glibc/src
-DESTDIR=$HOME/glibc/build
-CURDIR=`pwd`
+DESTDIR=$HOME/glibc/dest
+BUILDDIR=$HOME/glibc/build
 
 rm -rf $SRCDIR
 rm -rf $DESTDIR
@@ -16,9 +16,7 @@ cd $SRCDIR
 wget http://ftp.gnu.org/gnu/glibc/glibc-2.17.tar.gz
 tar -zxvf glibc-2.17.tar.gz
 
-cd $CURDIR
+cd $BUILDDIR
 $SRCDIR/glibc-2.17/configure --prefix=/usr
-
 make malloc
-
-#make install DESTDIR=${DESTDIR}
+make install DESTDIR=${DESTDIR}
