c9536fc034b75adec853c957ef1adc2d79878e40
lzueclipse <lzueclipse@163.com>
2015-08-27 04:04:03 -0400

    STL map does not release small chunks of memory after destruction(glibc malloc behavior).

--- c_cpp/0001/build.sh
+++ c_cpp/0001/build.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 set -e
 
+echo "Begin to compile mytest.cpp"
 rm -f *.o
 rm -f libcommon*
-
 g++ -fPIC -c -o common.o common.cpp
 g++ -shared -fPIC -o libcommon.so common.o
 #g++ -shared -fPIC -Wl,-soname,libcommon.so -o libcommon.so common.o
-
 #main.c
-g++ -Wl,-rpath=./ -o mytest mytest.cpp -L. -L./ptmalloc2  -lcommon -lcrypto
+g++ -Wl,-rpath=./ -o mytest mytest.cpp -L.  -lcommon -lcrypto
+echo "Complile mytest.cpp success"
 
-echo "Complile success"
+
+echo "Begin to compile ptmalloc2, but an old version".
+cd ptmalloc2
+make shared
+echo "Complile ptmalloc2 success"
