6e4105bc3e6104ded290145e310d5df8ad452e42
lzueclipse <lzueclipse@163.com>
2015-09-29 04:59:32 -0400

    Rewriting Chapter 2 Finger Print cache, use slab allocator to manage memory, use linked list | binary search tree | skip list as different search algorithm.

--- c_cpp/0002/common.c
+++ c_cpp/0002/common.c
@@ -43,6 +43,11 @@ void uint64_to_md5(uint64_t input, md5_digest_t *output )
 }
 
 
+/*
+ * 0  -- equal
+ * >0 -- more than
+ * <0 -- less than
+ */
 int32_t md5_digest_compare(const md5_digest_t *a, const md5_digest_t *b)
 {
     int32_t res = 0;
@@ -110,6 +115,26 @@ static __inline__ uint32_t get_digest_index(const cache_t *cache, const md5_dige
 static __inline__  cache_ll_node_t* get_root_node_slot(cache_t *cache, const md5_digest_t *digest)
 {
     return cache->cache_root[get_digest_index(cache, digest)];
+}
+
+static __inline__ cache_ll_node_t* get_node_under_slot(cache_ll_node_t *pn, const md5_digest_t *digest)
+{
+    while(pn)
+    {
+        int32_t ret = md5_digest_compare(&pn->digest, digest);
+
+        if(ret == 0)
+        {
+            /*found*/
+            break;
+        }
+        else
+        {
+            pn = pn->next;
+        }
+    }
+
+    return pn;
 }
 
 #endif
