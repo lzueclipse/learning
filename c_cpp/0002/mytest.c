ef4229b6831da2186bcedac98bc6427e04da00a1
lzueclipse <lzueclipse@163.com>
2015-10-12 06:07:34 -0400

    Rewriting Chapter 2 Finger Print cache, use slab allocator to manage memory, use linked list | binary search tree | skip list as different search algorithm.

--- c_cpp/0002/mytest.c
+++ c_cpp/0002/mytest.c
@@ -18,6 +18,7 @@ int main(int argc, char **argv)
     cache_t cache;
 
 #ifdef LINKEDLIST
+    cache_ll_node_t *node;
     ret = cache_init(&cache, 16, 2 * MEGABYTE, sizeof(cache_ll_node_t), MAXNUM + 100);
     if(ret != CACHE_INIT_OK)
     {
@@ -37,6 +38,23 @@ int main(int argc, char **argv)
     for(i = 0; i < MAXNUM; ++i) {
         uint64_to_md5(i, &my_fp);
         cache_insert(&cache, &my_fp, i);
+    }
+    my_end = time(NULL);
+    seconds = difftime(my_end, my_start);
+    printf("Insert all FPs into cache, cost time = %.f seconds\n", seconds);
+    printf("Output of 'top':\n");
+    output_top();
+    printf("---------------------------------------------------------------------------------------------\n");
+
+    my_start = time(NULL);
+    for(i = 0; i < MAXNUM; ++i) {
+        uint64_to_md5(i, &my_fp);
+        node = cache_lookup(&cache, &my_fp);
+
+        if(node != NULL)
+        {
+            printf("i = %" PRIu64 ", node->dcid = %" PRIu64 "\n", i, node->dcid);
+        }
     }
     my_end = time(NULL);
     seconds = difftime(my_end, my_start);
